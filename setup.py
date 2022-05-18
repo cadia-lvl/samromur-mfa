30249e9b8ca06f12355d14366bbe58a062ecedc3
Thomas Mestrou <t.mestrou@gmail.com>
2022-05-18 14:09:51 +0000

    Add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+    This is the setup file, allows this to be installed as a package.
+"""
+
+___author___ = "Thomas Mestrou"
+___license___ = "XXX"
+___copyright___ = "2022 Thomas Mestrou Reykjavík University"
+
+from setuptools import setup, find_packages
+
+setup(name="speech-corpus-tools", version="1.0", packages=find_packages())
