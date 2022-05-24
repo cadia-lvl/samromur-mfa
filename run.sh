411422be592f3445ae131ff9aa81bc5f885a080b
Thomas Mestrou <t.mestrou@gmail.com>
2022-05-24 16:46:29 +0000

    Modification done, still in work so it doesnt run

--- run.sh
+++ run.sh
@@ -8,17 +8,24 @@ NC='\033[0m'              # No Color
 
 # Getting the informations contained in info.json
 path_to_data="$(grep -o '"path_to_data": "[^"]*' info.json | grep -o '[^"]*$')"
+output="$(grep -o '"output_folder": "[^"]*' info.json | grep -o '[^"]*$')""/"
+model_path=$PWD"/""$output"$model_file
+
 dict_file="$(grep -o '"dictionary_file": "[^"]*' info.json | grep -o '[^"]*$')"
 lex_file="$(grep -o '"lexicon_file": "[^"]*' info.json | grep -o '[^"]*$')"
 model_file="$(grep -o '"MFA_model_name": "[^"]*' info.json | grep -o '[^"]*$')"
-model_path=$PWD"/"$model_file
-output_folder="$(grep -o '"output_folder": "[^"]*' info.json | grep -o '[^"]*$')"
 data_folder="$(grep -o '"data_folder": "[^"]*' info.json | grep -o '[^"]*$')"
 
-# If a log folder doesn't exist, it creates one
-if [ ! -d "$PWD""/log" ] 
+# If a log folder doesn't exist, it creates one. This folder will contain the log files that may be created in the different steps
+if [ ! -d "$PWD""$output""/log" ] 
 then
-    mkdir 'log'
+    mkdir $output'log'
+fi
+
+# If a out folder doesn't exist, it creates one. It will contain every files the 
+if [ ! -d "$PWD""$output""out" ] 
+then
+    mkdir $output'out'
 fi
 
 # Initialize the options "overwrite", "quiet" and "model"
@@ -61,21 +68,20 @@ if [ "${?}" -eq 1 ]
 then
     # In the end of every log file, we put the date and time of the last modification.
     echo "Last modification : ""$(date)" >> log/prep.log
-    printf " ${Red}FAILURE${NC} : Something went wrong. See the log/prep.log file for more information.\n "
+    printf "${Red}FAILURE${NC} : Something went wrong. See the log/prep.log file for more information.\n "
     exit 1
 else
-    printf " ${Green}SUCCESS${NC} : Preparation of folder.\n "
+    printf "${Green}SUCCESS${NC} : Preparation of folder.\n "
 fi
-# Creating .txt files corresponding to audio files in corresponding folders
 
 
 
 echo '========== Making lexicon ==========' 
 
 # If the user doesn't want to overwrite on existing file and if there is an existing file, it displays a message and go to the next step. 
-if [ $ov == false ] && [ -f $lex_file ]
+if [ $ov == false ] && [ -f "$output""$lex_file" ]
 then
-    printf " ${Yellow}NOTHING DONE${NC} : %s already existing.\n " "$lex_file"
+    printf "${Yellow}NOTHING DONE${NC} : %s already existing.\n " "$lex_file"
 else
     if $qu
     then
@@ -88,36 +94,36 @@ else
     if [ "${?}" -eq 1 ] 
     then
         echo "Last modification : ""$(date)" >> log/lex.log
-        printf " ${Red}FAILURE${NC} : Something went wrong. See the log/lex.log file for more information. \n "
+        printf "${Red}FAILURE${NC} : Something went wrong. See the log/lex.log file for more information. \n "
         exit 2
 
     else
-        printf " ${Green}SUCCESS${NC} : Lexicon made. \n"
+        printf "${Green}SUCCESS${NC} : Lexicon made. \n"
     fi
 fi
-# Creating the lexicon file of the data folder
+
 
 
 
 echo '========== Making dictionnary =========='
 
-if [ $ov == false ] && [ -f $dict_file ]
+if [ $ov == false ] && [ -f "$output""$dict_file" ]
 then
-    printf " ${Yellow}NOTHING DONE${NC} : %s already existing.\n " "$dict_file"
+    printf "${Yellow}NOTHING DONE${NC} : %s already existing.\n " "$output""$dict_file"
 else
     # We create a dictionary of phoneme  using the lexicon, the model 'ipd_clean_slt2018.mdl'.
-    python3 -m g2p --model ipd_clean_slt2018.mdl --apply "$lex_file" --encoding='utf-8' > "$dict_file" 2> log/dict.log
+    python3 -m g2p --model ipd_clean_slt2018.mdl --apply "$output""$lex_file" --encoding='utf-8' > "$output""$dict_file" 2> log/dict.log
 
     if [ "${?}" -eq 1 ] 
     then
         echo "Last modification : ""$(date)" >> log/dict.log
-        printf " ${Red}FAILURE${NC} : Something went wrong. See the log/dict.log file for more information.\n "
+        printf "${Red}FAILURE${NC} : Something went wrong. See the log/dict.log file for more information.\n "
         exit 3
     else
-        printf " ${Green}SUCCESS${NC} : Dictionnary made. \n"
+        printf "${Green}SUCCESS${NC} : Dictionnary made. \n"
     fi
 fi
-# Creating the dictionnary with the pre-trained model 'ipd_clean_slt2018.mdl'
+
 
 
 
@@ -125,47 +131,44 @@ echo '========== Validating the data =========='
 
 if $qu
 then
-    mfa validate --quiet "$path_to_data""$data_folder" "$dict_file" 2> log/val.log
+    mfa validate --quiet "$path_to_data""$data_folder" "$output""$dict_file" 2> log/val.log
 else
-    mfa validate "$path_to_data""$data_folder" "$dict_file" 2> log/val.log
+    mfa validate "$path_to_data""$data_folder" "$output""$dict_file" 2> log/val.log
 fi
 
 if [ "${?}" -eq 1 ] 
 then
     echo "Last modification : ""$(date)" >> log/val.log
-    printf " ${Red}FAILURE${NC} : Something went wrong. See the log/val.log file for more information.\n "
+    printf "${Red}FAILURE${NC} : Something went wrong. See the log/val.log file for more information.\n "
     exit 4
 else
-    printf " ${Green}SUCCESS${NC} : Data validated. \n"
+    printf "${Green}SUCCESS${NC} : Data validated. \n"
 fi    
 
 
+echo '========== Creating and Training the MFA model or align the file=========='
 
-echo '========== Creating and Training the MFA model =========='
-
-if [ $ov == false ] && [ -f $model_file".zip" ]
+if [ $ov == false ] && [ -f "$output""$model_file"".zip" ]
 then
-    printf " ${Yellow}NOTHING DONE${NC} : %s already created.\n " "$model_file"
+    printf "${Yellow}NOTHING DONE${NC} : %s already created.\n " "$output""$model_file"
 
 else
     if $qu
     then
-        mfa train --output_model_path "$model_path" --quiet --clean --overwrite "$path_to_data""$data_folder" "$dict_file" "$output_folder" 2> log/train.log
+        mfa train --output_model_path "$model_path" --quiet --clean --overwrite "$path_to_data""$data_folder" "$output""$dict_file" "$output""out" 2> log/train.log
     else
-        mfa train --output_model_path "$model_path" --clean --overwrite "$path_to_data""$data_folder" "$dict_file" "$output_folder" 2> log/train.log
+        mfa train --output_model_path "$model_path" --clean --overwrite "$path_to_data""$data_folder" "$output""$dict_file" "$output""out" 2> log/train.log
     fi
 
     if [ "${?}" -eq 1 ] 
     then
         echo "Last modification : ""$(date)" >> log/train.log
-        printf " ${Red}FAILURE${NC} : Something went wrong. See the log/train.log file for more information.\n "
+        printf "${Red}FAILURE${NC} : Something went wrong. See the log/train.log file for more information.\n "
         exit 5
     else
-        printf " ${Green}SUCCESS${NC} : Training finished. \n"
-        steps=$(( $steps + 1 ))
+        printf "${Green}SUCCESS${NC} : Training finished. \n"
     fi        
 fi
-# Training the model
 
 
 
@@ -175,11 +178,10 @@ python3 segmentation_check.py info.json 2> log/check.log
 if [ "${?}" -eq 1 ] 
 then
     echo "Last modification : ""$(date)" >> log/check.log
-    printf " ${Red}FAILURE${NC} : Something went wrong. See the log/check.log file for more information.\n "
+    printf "${Red}FAILURE${NC} : Something went wrong. See the log/check.log file for more information.\n "
     exit 6
 else
-    printf " ${Green}SUCCESS${NC} : Checking finished. \n"
+    printf "${Green}SUCCESS${NC} : Checking finished. \n"
 fi    
-# Checking if the alignment has been done in all files
 
-printf " ${Green}==================== Program finished without errors ====================${NC}\n "
+printf "${Green}==================== Program finished without errors ====================${NC}\n "
--- segmentation_check.py
+++ segmentation_check.py
@@ -3,10 +3,11 @@ import os, json, sys
 with open(sys.argv[1]) as fichierOptions:
     options = json.load(fichierOptions)
 
-output_folder = options['output_folder']
+output_folder = options['output_folder'] + "/out"
 
 # Creation of an error folder if it doesn't exist yet
-error_folder = options['empty_audio_user'] 
+error_folder = options['output_folder'] + "/" + options['empty_audio_user'] 
+error_file = error_folder + ".txt"
 if (error_folder not in os.listdir()) :
     os.mkdir(error_folder)
 
@@ -27,7 +28,7 @@ while (clean == False) :
                 if (folder_user not in os.listdir(error_folder)) :
                     os.system("cp -r " + options['path_to_data'] + options['data_folder'] + "/" + folder_user + " " + error_folder + "/" + folder_user)
 
-    f = open(options['empty_audio_user'] + '.txt', "w")
+    f = open(error_file, "w")
     f.write(str(empty_audio_user_list))
     f.close()
 
@@ -38,15 +39,5 @@ while (clean == False) :
     if (len(empty_audio_user_list) == 0) :
         clean = True
         os.system("rm -r " + error_folder)
-        os.system("rm " + options['empty_audio_user'] + ".txt")
+        os.system("rm " + error_file)
         break
-
-    # Depending on the percentage of errors, the user will maybe don't want to clean. 
-    r = input("Do you want to clean ? (y/n)\n")
-    if (r == 'y' or r == 'Y') :
-        bash_command_align = "mfa align --clean " + error_folder + " " + options['dictionary_file'] + " " + options["MFA_model_name"] + ".zip" + " output_2"
-        os.system(bash_command_align)
-    elif (r == 'n' or r == "N") :
-        clean = True
-    else : 
-        print("Wrong answer, say 'y' for yes or 'n' for no")
